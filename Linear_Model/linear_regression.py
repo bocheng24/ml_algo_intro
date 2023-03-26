51cd2b81da4b176efc0b2d8ca27864d2562b2155
bocheng24 <jeffcgp@hotmail.com>
2023-03-26 16:02:03 +0800

    single weighted and bias algo

--- Linear_Model/linear_regression.py
+++ Linear_Model/linear_regression.py
@@ -1,13 +1,30 @@
 import numpy as np
 
 class Linear_Regression():
+    
+
+    def predict(self, x, w, b):
+
+        m = x.shape[0]
+        f_wb = np.zeros(m)
+        
+        for i in range(m):
+            f_wb[i] = w * x[i] + b
+
+        return f_wb
 
     def __repr__(self):
         return 'Linear Regression Model'
 
 def main():
     lr = Linear_Regression()
-    print(lr)
+    x = np.array([1, 2])
+    y = np.array([300, 500])
+    w = 200
+    b = 100
+
+    f_wb = lr.predict(x, w, b)
+    print(f_wb)
 
 if __name__ == '__main__':
 
