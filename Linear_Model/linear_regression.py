1d5502231055c71b3729bb43edecd7144c7da5d5
bocheng24 <jeffcgp@hotmail.com>
2023-03-26 16:44:24 +0800

    cost function

--- Linear_Model/linear_regression.py
+++ Linear_Model/linear_regression.py
@@ -2,6 +2,14 @@ import numpy as np
 
 class Linear_Regression():
     
+    def compute_cost(self, x, y, w, b):
+
+        f_wb = self.predict(x, w, b)
+        m = y.shape[0]
+        
+        total_cost = np.sum((y - f_wb) ** 2) / (2 * m)
+
+        return total_cost
 
     def predict(self, x, w, b):
 
@@ -20,12 +28,11 @@ def main():
     lr = Linear_Regression()
     x = np.array([1, 2])
     y = np.array([300, 500])
-    w = 200
+    w = 100
     b = 100
 
-    f_wb = lr.predict(x, w, b)
-    print(f_wb)
+    cost = lr.compute_cost(x, y, w, b)
+    print(cost)
 
 if __name__ == '__main__':
-
     main()
