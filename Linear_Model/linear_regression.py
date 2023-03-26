1705d3bd6a6ae0be1227cf1df14562e041f721b3
bocheng24 <jeffcgp@hotmail.com>
2023-03-26 15:13:37 +0800

    create linear regression class

---
+++ Linear_Model/linear_regression.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class Linear_Regression():
+
+    def __repr__(self):
+        return 'Linear Regression Model'
+
+def main():
+    lr = Linear_Regression()
+    print(lr)
+
+if __name__ == '__main__':
+
+    main()
